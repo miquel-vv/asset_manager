c33581bf490e7fab03984c21b386df5d99c47d11
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2020-06-17 09:42:11 +0200

    Create base Asset and Crypto class

---
+++ asset_manager/assets/Asset.py
@@ -0,0 +1,13 @@
+from ..mappers import PriceMapper
+from ..connectors import KrakenConnector
+
+class Asset:
+    def __init__(self, asset_id):
+        self.asset_id = asset_id
+        self.price_mapper = PriceMapper()
+        self.prices = price_mapper.get_prices(asset_id)
+        self.last_saved_price = self.get_last_price_date()
+
+    def get_last_price_date(self):
+        return self.prices.index[-1]
+    
