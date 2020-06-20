de84914786581b9d6e9fdf4744514e08fe0d9f69
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2020-06-20 14:13:08 +0200

    Add

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asset_manager", # Replace with your own username
+    version="0.0.1",
+    author="Miquel Vande Velde",
+    author_email="miquel.vandevelde@gmail.com",
+    description="Personal package of asset manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "pandas",
+        "psycopg2",
+        "SQLAlchemy"
+    ]
+)
